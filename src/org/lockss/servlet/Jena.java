1ce22075248df0dff2608f9e7d69937f6ac40f72
Morgan Brown <brown.mhg@gmail.com>
2013-04-25 23:43:50 -0700

    Further refine the Jena Query Interface

--- src/org/lockss/servlet/Jena.java
+++ src/org/lockss/servlet/Jena.java
@@ -8,7 +8,7 @@ import org.mortbay.html.*;
 
 public class Jena extends LockssServlet {
 	
-	private final String HEADING = "Enter a SPARQL query and view the results.";
+	private final String HEADING = "Enter a SPARQL query, then press enter";
 	
 	private final String NAME_QUERY_PARAMETER = "name";
 
@@ -22,28 +22,53 @@ public class Jena extends LockssServlet {
 		displayPage();
 	}
 	
-	private void displayPage() throws IOException{
+	private void displayPage() throws IOException {
 		Page page = newPage();
-	    ServletUtil.layoutExplanationBlock(page, HEADING);
-	    layoutErrorBlock(page);
+		page.add(createHeading());
 	    page.add(createQueryForm());
+	    page.add(createResultsContainer());
 		endPage(page);
 	}
 	
-	private Element createQueryForm(){
+	private Element createHeading() {
+		Composite comp = new Composite();
+		
+		comp.add("<center>");
+		comp.add("<h2>" + HEADING +"</h2>");
+		comp.add("</center>");
+		
+		return comp;
+	}
+	
+	private Element createQueryForm() {
 	    Form form = new Form(srvURL(myServletDescr()));
 	    form.method("POST");
 	    
 	    form.add("<center>");
 	    TextArea query = new TextArea(NAME_QUERY_PARAMETER);
 	    query.setSize(70, 10);
+	    query.attribute("style", "border: 1px solid black; resize: none;");
 	    form.add(query);
+	    
+	    form.add("<br/>");
+	    form.add("<input type=\"submit\" value=\"Query\" style=\"width: 512px;\"/>");
+	    
 	    form.add("</center>");
 	    
 	    return form;
 	}
 	
-	private Element createResultsContainer(){
-		return null;
+	private Element createResultsContainer() {
+		Composite comp = new Composite();
+		comp.add("<center>");
+		comp.add("<h2>Results</h2>");
+		comp.add("<div style=\"width: 512px; height: 136px; overflow: scroll; border: 1px solid black;\">");
+		
+		Table results = new Table();
+		comp.add(results);
+		
+		comp.add("</div></center>");
+		
+		return comp;
 	}
 }
